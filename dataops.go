2e0a75ed2d469dd231511c3676f266f38de20563
philhofer <phofer@umich.edu>
2014-01-23 18:44:19 -0500

    Added Sort interface for BiVariateData

--- dataops.go
+++ dataops.go
@@ -1,5 +1,8 @@
 package vec
 
+import "math"
+import "sort"
+
 /*
 Discreet Data Operations
 
@@ -8,22 +11,73 @@ Discreet Data Operations
 
 */
 
+//Struct for bivariate data (xs, ys)
 type BiVariateData struct {
-	x []float64
-	y []float64
+	Xs []float64
+	Ys []float64
+	isSorted bool
 }
 
-type Interpolation interface {
-	//Interpolation value
-	f(x float64)
+func MakeBiVariateData(xs []float64, ys []float64) BiVariateData {
+	out := BiVariateData{xs, ys, false}
+	out.Sort()
+	return out
+}
+
+func (b BiVariateData) Len() int{
+	return len(b.Xs)
+}
+
+func (b BiVariateData) Less(i, j int) bool {
+	if b.Xs[i] < b.Xs[j] {
+		return true
+	} else {
+		return false
+	}
+}
+
+func (b BiVariateData) Swap(i, j int) {
+	b.Xs[i], b.Xs[j] = b.Xs[j], b.Xs[i]
+	b.Ys[i], b.Ys[j] = b.Ys[j], b.Ys[i]
+	return
+}
+
+func (b BiVariateData) Sort() {
+	//edge case 1
+	if b.isSorted {
+		return
+	}
+
+	//edge case 2
+	if sort.Float64sAreSorted(b.Xs) {
+		b.isSorted = true
+		return
+	}
+
+	sort.Sort(b)
+
+	b.isSorted = true
+}
+
+func (b BiVariateData) findNearestX(x float64) int {
+	if !b.isSorted {b.Sort()}
 	
-	//First derivative
-	df(x float64)
+	//edge cases
+	if x > b.Xs[len(b.Xs)] { return len(b.Xs)-1 }
+	if x < b.Xs[0] { return 0 }
 
-	//Third derivative
-	ddf(x float64)
+	out := 0
+	for b.Xs[out] < x {
+		out++
+	}
+	
+	if math.Abs(b.Xs[out]-x) > math.Abs(b.Xs[out+1]) {
+		out++
+	}
+
+	return out
+	
 }
-
 
 /* 
 func CubicSpline(data BiVariateData) Interpolation {}
